573a7e74a26e1dc346c8ace6dc7e2e63289065cb
tejas <joshi.tejas0705@gmail.com>
2022-04-19 11:44:47 +0530

    added admin view

---
+++ app/src/main/java/com/example/appointement/AdminViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.appointement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view);
+    }
+}
